d81850c2c4dbb6becb9492adb220a59c2e26d0c7
Hashmath-19 <84575247+Hashmath-19@users.noreply.github.com>
2022-01-24 13:51:31 +0530

    Update index.js
    added refresh me

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var randomDiceImage2 = "dice" +randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2)
 
 if(randomNumber1>randomNumber2){
-  document.querySelector("h1").innerHTML = " Player 1 Wins"
+  document.querySelector("h1").innerHTML = " Player 1 Wins! Refresh for new game"
 }
 else if(randomNumber1<randomNumber2){
-  document.querySelector("h1").innerHTML = "Player 2 Wins"
+  document.querySelector("h1").innerHTML = "Player 2 Wins! Refresh for new game"
 }
 else{
-  document.querySelector("h1").innerHTML = "Draw!"
+  document.querySelector("h1").innerHTML = "Draw! Refresh for new game"
 }
